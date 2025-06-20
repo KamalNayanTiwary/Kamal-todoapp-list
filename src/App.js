845120262fabcc5e2ec969af29b55863c29adb99
Kamal Nayan Tiwary <kamalnayantiwary73@gmail.com>
2025-06-20 17:50:34 +0530

    Final UI fix: centered wallpaper and responsive layout

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
-import React, { useState } from 'react'
-import "./App.css"
-import TodoInput from './components/TodoInput'
-import Todolist from './components/TodoList';
+import React, { useState } from "react";
+import "./App.css";
+import TodoInput from "./components/TodoInput";
+import Todolist from "./components/TodoList";
 function App() {
-  const [listTodo,setListTodo]=useState([]);
-  let addList = (inputText)=>{
-    if(inputText!=='')
-      setListTodo([...listTodo,inputText]);
-  }
-  const deleteListItem = (key)=>{
+  const [listTodo, setListTodo] = useState([]);
+  let addList = (inputText) => {
+    if (inputText !== "") setListTodo([...listTodo, inputText]);
+  };
+  const deleteListItem = (key) => {
     let newListTodo = [...listTodo];
-    newListTodo.splice(key,1)
-    setListTodo([...newListTodo])
-  }
+    newListTodo.splice(key, 1);
+    setListTodo([...newListTodo]);
+  };
+
   return (
-    <div className="main-container">
-      <div className="center-container">
-        <TodoInput addList={addList}/>
-        <h1 className="app-heading">TODO</h1>
-        <hr/>
-        {listTodo.map((listItem,i)=>{
-          return (
-            <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
-          )
-        })}
+    <div
+      style={{
+        backgroundImage: "url('/images/wallpaper.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "48% center",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "20px",
+        overflow: "hidden",
+      }}
+    >
+      <div
+        className="center-container"
+        style={{
+          backgroundColor: "rgba(0, 0, 0, 0.6)",
+          padding: "30px",
+          borderRadius: "15px",
+          color: "white",
+          width: "90%",
+          maxWidth: "400px",
+        }}
+      >
+        <h1 className="app-heading" style={{ textAlign: "center" }}>
+          📝K.T-TODO
+        </h1>
+        <TodoInput addList={addList} />
+        <hr />
+        {listTodo.map((listItem, i) => (
+          <Todolist
+            key={i}
+            index={i}
+            item={listItem}
+            deleteItem={deleteListItem}
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
